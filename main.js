a2c959a601322ca4a08da0ad69b6825c59029f89
Pantyil <69921450+le123456@users.noreply.github.com>
2025-08-26 22:40:09 -0300

    Initial commit from your app

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
 console.log("main.js loaded");
 
-const consultantData = {
-  name: "Thiago Eduard Santos",
-  email: "thiagoeduardosantossyn@gmail.com",
-  phone: "(62) 99837-5214",
-  address: "Av. T-63, 3421, Sala 1208, Setor Bueno, Goiânia – GO",
-  cep: "74230-032",
-  cnpj: "69.447.071/0001-33"
-};
+import { consultantData } from './data.js';
 
 // Smooth scrolling for anchor links on the same page
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
